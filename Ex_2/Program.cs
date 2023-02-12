cc016319de0739159b83cc16018264ae98589dad
Ivan Surguchev <ivisurg@yandex.ru>
2023-02-12 21:39:28 +0300

    Ex_2 completed

--- Ex_2/Program.cs
+++ Ex_2/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Write("Введите первое число > ");
+string inputValue1 = Console.ReadLine();
+
+Console.Write("Введите второе число > ");
+string inputValue2 = Console.ReadLine();
+
+int value1 = Convert.ToInt32(inputValue1);   //переводим первое число в целочисленное значение
+int value2 = Convert.ToInt32(inputValue2);   //переводим второе число в целочисленное значение
+
+if (value1>value2)
+{
+    System.Console.WriteLine($"Число {value1} - большее, а число {value2} - меньшее");
+}
+else
+{
+    System.Console.WriteLine($"Число {value2} - большее, а число {value1} - меньшее");
+}
